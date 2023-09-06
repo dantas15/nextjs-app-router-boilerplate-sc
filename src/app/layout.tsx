eb43ff92713e87aac2bc67c54d2bc7cc4b9b1b03
Gustavo Dantas Galote <gusgalot3g@gmail.com>
2023-09-06 19:41:20 -0300

    style: run format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Next.js boilerplate',
-  description: 'Custom Next.js boilerplate with Typescript, ESLint, Prettier, Husky, Lint-staged, and more.',
-}
+  description:
+    'Custom Next.js boilerplate with Typescript, ESLint, Prettier, Husky, Lint-staged, and more.',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
